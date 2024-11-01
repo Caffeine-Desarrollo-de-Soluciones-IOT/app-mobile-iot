bfd446ab82e78f066c0fd94a11467ae42c3a8fb8
deyson63 <u20201a991@upc.edu.pe>
2024-11-01 16:37:15 -0500

    fix: loggin

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:app_mobile_iot/core/widgets/bottom_nav_bar.dart';
-import 'package:app_mobile_iot/features/home/presentation/pages/home_page.dart';
+//import 'package:app_mobile_iot/core/widgets/bottom_nav_bar.dart';
+//import 'package:app_mobile_iot/features/home/presentation/pages/home_page.dart';
 import 'package:app_mobile_iot/features/login/app.dart';
 //import 'package:app_mobile_iot/features/login/screens/login_screen.dart';
 import 'package:flutter/material.dart';
